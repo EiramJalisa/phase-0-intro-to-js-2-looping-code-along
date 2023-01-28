274fe74c160e67b69a70883d2f9f77b87515942f
+ <+>
2023-01-28 10:55:00 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(array, string){
+    const messages = [] 
+    for (let i = 0; i < array.length; i++) {
+      messages.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+     debugger;
+    }
+    return messages;
+  }
+  
+  function countDown (){
+    let countDown = 0
+    while (countDown < 11){
+      console.log(countDown++)
+    }
+  }
